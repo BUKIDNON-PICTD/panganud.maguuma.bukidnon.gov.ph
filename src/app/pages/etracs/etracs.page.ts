007d21b0dfa82c28939eea2b8e01dc3f38475ae5
Hoyosjade27 <hoyosjade27@gmail.com>
2019-06-20 12:42:38 +0800

    update etracspage

--- src/app/pages/etracs/etracs.page.ts
+++ src/app/pages/etracs/etracs.page.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { EtracsService } from 'src/app/services/etracs.service';
-import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 import { Socket } from 'ng-socket-io';
+import { environment } from '../../../environments/environment';
+import { FarmerService } from 'src/app/services/farmer.service';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-etracs',
@@ -9,29 +15,47 @@ import { Socket } from 'ng-socket-io';
   styleUrls: ['./etracs.page.scss'],
 })
 export class EtracsPage implements OnInit {
-  dashdata: any;
+  dashrpu: any;
   params: any;
   name: Object;
-
+  public barChartOptions: ChartOptions;
+  public barChartLabels: Label[];
+  public barChartData: ChartDataSets[];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [pluginDataLabels];
   constructor(
     private socket: Socket,
     public etracsService: EtracsService
+    
     ) {
-    this.dashdata = [];
+    this.dashrpu = [];
     this.params = {
-      reciever : 'rufy',
+      reciever : 'jade',
       sender : environment.clientcode,
-      servicename	: 'TagabukidHRMISDashReportService',
-      methodname	: 'getHello',
-      message : 'Ralph'
+      servicename	: 'EtracsDashReportService',
+      methodname	: 'getRpudash'
     };
 
     this.socket.on('serverresponse', (data) => {
       if (data.servicename === this.params.servicename && data.methodname === this.params.methodname) {
-        this.name = data.result;
+        this.dashrpu = data.result;
       }
-     
     });
+
+    this.barChartLabels = this.dashrpu.map(function(a) {
+      return  a.name;
+    });
+    this.barChartOptions = {
+      responsive: true,
+      scales: { xAxes: [{}], yAxes: [{}] },
+      plugins: {
+        datalabels: {
+          anchor: 'end',
+          align: 'end',
+        }
+      }
+    };
    }
 
   ngOnInit() {
