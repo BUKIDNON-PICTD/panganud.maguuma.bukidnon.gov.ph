f6ad0d8a3249b4f8bf70505353b40807a144d86e
PORA <bylgrad@gmail.com>
2019-06-28 08:39:09 +0800

    etracs dashdata 1

--- src/app/pages/etracs/etracs.page.ts
+++ src/app/pages/etracs/etracs.page.ts
@@ -3,12 +3,15 @@ import { EtracsService } from 'src/app/services/etracs.service';
 import { environment } from 'src/environments/environment';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Socket } from 'ng-socket-io';
-import { Label } from 'ng2-charts';
+import { Label, SingleDataSet } from 'ng2-charts';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import { AuthService } from 'src/app/services/auth.service';
-import { ToastController } from '@ionic/angular';
+import { ToastController, IonSlides } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { FarmerService } from 'src/app/services/farmer.service';
+import { ViewChild } from '@angular/core';
+import { IonSlide } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-etracs',
@@ -16,48 +19,120 @@ import { FarmerService } from 'src/app/services/farmer.service';
   styleUrls: ['./etracs.page.scss'],
 })
 export class EtracsPage implements OnInit {
+
+  slideOptions = {
+    initialSlide: 1,
+    speed: 400,
+  };
+
   dashrpu: any;
   params: any;
   name: Object;
+
+
   public barChartOptions: ChartOptions;
   public barChartLabels: Label[];
   public barChartData: ChartDataSets[];
+  // public barChartData2: ChartDataSets[];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [pluginDataLabels];
+
+
+  public barChartLabelsRPUDash: Label[];
+  public barChartDataRPUDash: ChartDataSets[];
+  public barChartDataRPUDash2: ChartDataSets[];
+
+  public polarAreaChartLabels: Label[];
+  public polarAreaChartData: SingleDataSet;
+  public polarAreaLegend = true;
+  public polarAreaChartType: ChartType = 'polarArea';
+
+
+  slidesDidLoad(slides: IonSlides) {
+    slides.startAutoplay();
+  }
+
   constructor(
     private socket: Socket,
     public etracsService: EtracsService
-    
+
     ) {
     this.dashrpu = [];
     this.params = {
-      reciever : 'jade',
+      reciever : 'pora',
       sender : environment.clientcode,
       servicename	: 'EtracsDashReportService',
-      methodname	: 'getRpudash'
+      methodname: 'dashRPU'
     };
 
     this.socket.on('serverresponse', (data) => {
       if (data.servicename === this.params.servicename && data.methodname === this.params.methodname) {
         this.dashrpu = data.result;
       }
-    });
 
-    this.barChartLabels = this.dashrpu.map(function(a) {
-      return  a.name;
-    });
-    this.barChartOptions = {
-      responsive: true,
-      scales: { xAxes: [{}], yAxes: [{}] },
-      plugins: {
-        datalabels: {
-          anchor: 'end',
-          align: 'end',
+      this.barChartLabelsRPUDash = this.dashrpu.rpudash.map(function (a) {
+        return a.LGU;
+      });
+      this.barChartDataRPUDash = [
+        {
+          data: this.dashrpu.rpudash.map(function (a) {
+            return a.LAND;
+          }),
+          label: 'LAND'
+        },
+        {
+          data: this.dashrpu.rpudash.map(function (a) {
+            return a.BLDG;
+          }),
+          label: 'BLDG'
         }
-      }
-    };
-   }
+      ];
+      this.barChartDataRPUDash2 = [
+        {
+          data: this.dashrpu.rpudash.map(function (a) {
+            return a.MACH;
+          }),
+          label: 'MACHINERY'
+        },
+        {
+          data: this.dashrpu.rpudash.map(function (a) {
+            return a.PLANTTREE;
+          }),
+          label: 'PLANT TREE'
+        },
+        {
+          data: this.dashrpu.rpudash.map(function (a) {
+            return a.MISC;
+          }),
+          label: 'MISC'
+        }
+      ];
+
+      // this.barChartLabels = this.dashrpu.rpudashtotalhr.map(function (a) {
+      //   return a.LGU;
+      // });
+      this.barChartOptions = {
+        responsive: true,
+        scales: { xAxes: [{}], yAxes: [{}] },
+        plugins: {
+          datalabels: {
+            anchor: 'end',
+            align: 'end',
+          }
+        }
+      };
+
+      // this.polarAreaChartLabels = this.dashrpu.rpudashtotalhr[0].map(function (a) {
+      //   return a.LGU;
+      // });
+      // this.polarAreaChartData = [
+      //   this.dashrpu.rpudashtotalhr[0].map(function (a) {
+      //     return a.LAND;
+      //   })
+      // ];
+    });
+}
 
   ngOnInit() {
     this.socket.emit('serverrequest', this.params);
